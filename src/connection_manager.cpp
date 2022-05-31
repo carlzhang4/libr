d5abf8f098811997dcd00deb8704cb0c00441d8a
carlzhang4 <2470871694@qq.com>
2022-05-31 13:43:05 +0800

    add tcp sync to connection_manager

--- src/connection_manager.cpp
+++ src/connection_manager.cpp
@@ -1,5 +1,71 @@
 #include "connection_manager.hpp"
 #include "util.hpp"
-int start_cm_server(string ip){
-	
+void sync(void* sendData, void** receivedData, uint64_t sendDataLength) {
+    addrinfo hints{};
+    // Initialize Server Info
+    hints.ai_socktype = SOCK_STREAM;
+    addrinfo* serverAddress{nullptr};
+    getaddrinfo(SERVER_IP, SERVER_PORT, &hints, &serverAddress);
+    // Connect To Server
+    auto socketToServer = socket(serverAddress->ai_family, serverAddress->ai_socktype, serverAddress->ai_protocol);
+    auto isConnected = connect(socketToServer, serverAddress->ai_addr, serverAddress->ai_addrlen);
+    freeaddrinfo(serverAddress);
+    // Send Sync Data
+    send(socketToServer, sendData, sendDataLength, 0);
+    uint64_t readBytes = 0;
+    uint64_t receivedDataLength = (CLIENT_NUMBER - 1) * sendDataLength;
+    *receivedData = reinterpret_cast<void*>(new uint8_t[receivedDataLength]);
+    // Receive Sync Data
+    while (readBytes != receivedDataLength) {
+        readBytes += recv(socketToServer, (*receivedData) + readBytes, receivedDataLength - readBytes, 0);
+    }
+    close(socketToServer);
+}
+
+void syncServer(uint64_t receivedDataLength) {
+    addrinfo hints{};
+    // Initialize Server Info
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    addrinfo* serverBindAddress{nullptr};
+    getaddrinfo(NULL, SERVER_PORT, &hints, &serverBindAddress);
+    auto socketListen = socket(serverBindAddress->ai_family, serverBindAddress->ai_socktype, serverBindAddress->ai_protocol);
+    // Bind The Socket
+    auto isBound = bind(socketListen, serverBindAddress->ai_addr, serverBindAddress->ai_addrlen);
+    // Start Listening Port
+    auto isListening = listen(socketListen, 1024);
+    decltype(socketListen) socketClients[CLIENT_NUMBER]{};
+    void* syncData[CLIENT_NUMBER]{};
+    freeaddrinfo(serverBindAddress);
+    // Receive Sync Data
+    for (int i = 0; i < CLIENT_NUMBER; i++) {
+        // Accept Connection
+        sockaddr_storage connectionAddress;
+        socklen_t connectionLength = sizeof(connectionAddress);
+        auto socketConnection = accept(socketListen, (sockaddr*)&connectionAddress, &connectionLength);
+        socketClients[i] = socketConnection;
+        syncData[i] = reinterpret_cast<void*>(new uint8_t[receivedDataLength]);
+        // Receive Sync Data
+        uint64_t readBytes = 0;
+        while (readBytes != receivedDataLength) {
+            readBytes += recv(socketConnection, syncData[i]+readBytes, receivedDataLength-readBytes, 0);
+        }
+    }
+
+    // Sent Sync Data
+    for (int i = 0; i < CLIENT_NUMBER; i++) {
+        auto socketConnection = socketClients[i];
+        for (int j = 0; j < CLIENT_NUMBER; j++) {
+            if (j != i) {
+                send(socketConnection, syncData[j], receivedDataLength, 0);
+            }
+        }
+        close(socketConnection);
+    }
+
+    close(socketListen);
+    for (int i = 0; i < CLIENT_NUMBER; i++) {
+        delete [] reinterpret_cast<uint8_t*>(syncData[i]);
+    }
 }
--- src/connection_manager.hpp
+++ src/connection_manager.hpp
@@ -1,7 +1,28 @@
 #ifndef __CONNECTION_MANAGER_HPP__
 #define __CONNECTION_MANAGER_HPP__
-#include <iostream>
+#include <cstring>
 #include <string>
-using namespace std;
-int start_cm_server(string ip); // start connection manager server
+#include <set>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <map>
+#include <atomic>
+#include <ctime>
+#include "util.hpp"
+#define HOST_NAME_LENGTH            256
+#define ADDRESS_BUFFER_LENGTH       100
+
+#define SERVER_IP               "127.0.0.1"
+#define SERVER_PORT             "6666"
+#define CLIENT_NUMBER           4
+
+void sync(void* sendData, void** receivedData, uint64_t sendDataLength);
+void syncServer(uint64_t receivedDataLength);
 #endif
