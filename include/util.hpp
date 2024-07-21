d914d0750c46448f6bff7fa26678bed3da455657
Chen Xuzheng <1092889706@qq.com>
2024-07-21 17:49:01 +0800

    feat: refactor dir struct for add more test setup

--- include/util.hpp
+++ include/util.hpp
@@ -5,13 +5,12 @@
 #include <string>
 #include <infiniband/verbs.h>
 #include <sys/mman.h>
-#include <thread>
-#include <vector>
+#include <bits/stdc++.h>
 #include <assert.h>
 #include <numaif.h>
 #include <numa.h>
 #include <unistd.h>
-#include <mutex>
+#include <execinfo.h>
 
 using namespace std;
 
@@ -72,6 +71,112 @@ public:
 	int tx_depth;
 	int rx_depth;
 };
+
+
+#define _unused(x) ((void)(x)) // Make production build happy
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+#define Ki(x) (static_cast<size_t>(x) * 1000)
+#define Mi(x) (static_cast<size_t>(x) * 1000 * 1000)
+#define Gi(x) (static_cast<size_t>(x) * 1000 * 1000 * 1000)
+
+#define KB(x) (static_cast<size_t>(x) << 10)
+#define MB(x) (static_cast<size_t>(x) << 20)
+#define GB(x) (static_cast<size_t>(x) << 30)
+
+#define min_(x, y) x < y ? x : y
+
+#define max_(x, y) x > y ? x : y
+
+#define min_t(type, x, y) ({            \
+    type __min1 = (x);            \
+    type __min2 = (y);            \
+    __min1 < __min2 ? __min1: __min2; })
+
+#define max_t(type, x, y) ({            \
+    type __max1 = (x);            \
+    type __max2 = (y);            \
+    __max1 > __max2 ? __max1: __max2; })
+
+#define SWAP(x, y)             \
+    do                         \
+    {                          \
+        typeof(x) ____val = x; \
+        x = y;                 \
+        y = ____val;           \
+    } while (0)
+
+#define is_log2(v) (((v) & ((v)-1)) == 0)
+
+
+static size_t round_up(size_t num, size_t factor) {
+	return num + factor - 1 - (num + factor - 1) % factor;
+}
+
+static void print_bt() {
+	void *array[10];
+	size_t size;
+
+	// get void*'s for all entries on the stack
+	size = static_cast<size_t>(backtrace(array, 10));
+
+	// print out all the frames to stderr
+	backtrace_symbols_fd(array, size, STDERR_FILENO);
+}
+
+static inline void rt_assert(bool condition, const std::string &throw_str, char *s) {
+	if (unlikely(!condition)) {
+		print_bt();
+		throw std::runtime_error(throw_str + std::string(s));
+	}
+}
+
+/// Check a condition at runtime. If the condition is false, throw exception.
+static inline void rt_assert(bool condition, const char *throw_str) {
+	if (unlikely(!condition)) {
+		print_bt();
+		throw std::runtime_error(std::string(throw_str));
+	}
+}
+
+/// Check a condition at runtime. If the condition is false, throw exception.
+static inline void rt_assert(bool condition, const std::string &throw_str) {
+	if (unlikely(!condition)) {
+		print_bt();
+		throw std::runtime_error(throw_str);
+	}
+}
+
+/// Check a condition at runtime. If the condition is false, throw exception.
+/// This is faster than rt_assert(cond, str) as it avoids string construction.
+static inline void rt_assert(bool condition) {
+	if (unlikely(!condition)) {
+		print_bt();
+		throw std::runtime_error("Error");
+	}
+}
+
+static inline void rt_assert(bool condition, const char *file_name, int line) {
+	if (unlikely(!condition)) {
+		print_bt();
+		throw std::runtime_error(std::string(file_name) + ":" + std::to_string(line));
+	}
+}
+
+template <typename... Args>
+std::string string_format(const std::string &format, Args... args) {
+	int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) + 1; // Extra space for '\0'
+	if (size_s <= 0) {
+		throw std::runtime_error("Error during formatting.");
+	}
+	auto size = static_cast<size_t>(size_s);
+	std::unique_ptr<char[]> buf(new char[size]);
+	std::snprintf(buf.get(), size, format.c_str(), args...);
+	return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
+}
+
+
 
 #define ALLOCATE(var,type,size)                                     \
 { if((var = static_cast<type*>(malloc(sizeof(type)*(size)))) == NULL)        \
